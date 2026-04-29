530edc5bcccd1a58935ee35ec7c1abbf64a5e2f1
Lidobix <pipoflutepouet@gmail.com>
2026-04-29 23:52:19 +0200

    chore: setup linters & hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,7 @@
+import 'react-native-reanimated';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
@@ -11,13 +10,13 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix'],
-  '*.{json,md,css,scss}': ['prettier --write'],
-};
